5f42a9d16e409aa39d40c56b68de373acc9b2f37
Nick Rutten <nickrttn@users.noreply.github.com>
2021-01-15 13:50:28 +0100

    Upgrade preact to the latest version

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 const withPreact = require("next-plugin-preact");
 
-require("fs").copyFileSync(
-  "./node_modules/preact/compat/jsx-runtime.js",
-  "./node_modules/preact/compat/jsx-dev-runtime.js"
-);
-
 module.exports = withPreact({
   reactStrictMode: true,
 });
